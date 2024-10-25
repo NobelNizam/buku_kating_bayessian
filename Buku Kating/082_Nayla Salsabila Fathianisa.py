e39840c5aa590584512e99e9a4c9194e0e8b52d1
naylasalsabila15 <nayla.123450082@student.itera.ac.id>
2024-10-25 21:28:40 +0700

    nayss

--- Buku Kating/082_Nayla Salsabila Fathianisa.py
+++ Buku Kating/082_Nayla Salsabila Fathianisa.py
@@ -633,7 +633,7 @@ elif menu == "Departemen MIKFES":
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
         ]
-        data_list = [
+        data_list = [  
             {
                 "nama": "Kakak 1",
                 "nim": "122450000",
