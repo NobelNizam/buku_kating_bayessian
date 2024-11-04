f3b768772c272d397552a9c78721e8a54bf75090
NobelNizam <username.nnf@gmail.com>
2024-11-04 21:24:25 +0700

    new baleg

--- Buku Kating/117_Nobel Nizam.py
+++ Buku Kating/117_Nobel Nizam.py
@@ -184,13 +184,13 @@ elif menu == "Baleg":
             "https://drive.google.com/uc?export=view&id=1hpLjgFJZscnAlnyVi51hj4wYIctEDJQE",#Kak Annisa
             "https://drive.google.com/uc?export=view&id=1o3-18_m56I74F1Hpd5BFXeVRxzG-Ybos",#Kak Wulan
             "https://drive.google.com/uc?export=view&id=1cdIFl-zVh4Bs-_FNdny38zQqHmWuwQLN",#Kak Anisa Dini 
+            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",#Kak Claudhea .
             "https://drive.google.com/uc?export=view&id=18phQCp8MbPv1_V3ahtLAbbnk5ouacJMH",#Bg Feryadi
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",#Kak Renisha .
             "https://drive.google.com/uc?export=view&id=1Xjiy3S_3pvg5MWQ8-h0teeiIKKA9WlWh",#Bg Mirzan 
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",#Kak Claudhea .
-            "https://drive.google.com/uc?export=view&id=1138Lxhdg3s3qHG1FAipa4jJpNzv0zJ8t",#Bg Fahrul
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",#Kak Anisa Fitri .
             "https://drive.google.com/uc?export=view&id=1hPUw2h2C9s3woS68mR0QdFMIHt9js1rL",#Kak Dhea 
+            "https://drive.google.com/uc?export=view&id=1138Lxhdg3s3qHG1FAipa4jJpNzv0zJ8t",#Bg Fahrul
             "https://drive.google.com/uc?export=view&id=1qimUJvycTzg9yieFMOlwkcw4BufW28m9",#Kak Berliana 
             "https://drive.google.com/uc?export=view&id=1tL7ickfswEb6Ge6ooxtFqOgtg_jbnO4L",#Bg Jere
         ]
@@ -251,28 +251,6 @@ elif menu == "Baleg":
                 "pesan": "untung ketemu ya kak, lain kali lebih hati-hati yaa"
             },
             {
-                "nama": "Muhammad Fahrul Aditya",
-                "nim": "121450156",
-                "umur": "22",
-                "asal": "Surakarta",
-                "alamat": "Sukarame",
-                "hobbi": "Badminton, melukis, minum kopo",
-                "sosmed": "@shrul.pdf",
-                "kesan": "bang fahrul saya kira abang prodi elektro",  
-                "pesan": "next kita foto lagi bang saat abang wisuda!"
-            },
-            {
-                "nama": "Anisa Fitriyani",
-                "nim": "122450019",
-                "umur": "19",
-                "asal":"Bandar Lampung",
-                "alamat": "Bernung, Pesawaran",
-                "hobbi": "Menonton drakor",
-                "sosmed": "@ansftynn_",
-                "kesan": "seruuu",  
-                "pesan": "semangat kuuliahnya jangan malas kak"
-            },
-            {
                 "nama": "Feryadi Yulius",
                 "nim": "122450087",
                 "umur": "20",
@@ -306,6 +284,17 @@ elif menu == "Baleg":
                 "pesan": "abang jangan demis pokoknya!"
             },
             {
+                "nama": "Anisa Fitriyani",
+                "nim": "122450019",
+                "umur": "19",
+                "asal":"Bandar Lampung",
+                "alamat": "Bernung, Pesawaran",
+                "hobbi": "Menonton drakor",
+                "sosmed": "@ansftynn_",
+                "kesan": "seruuu",  
+                "pesan": "semangat kuuliahnya jangan malas kak"
+            },
+            {
                 "nama": "Dhea Amelia Putri",
                 "nim": "122450004",
                 "umur": "120",
@@ -315,6 +304,17 @@ elif menu == "Baleg":
                 "sosmed": "@myrrinn",
                 "kesan": "kak dhea lucu banget waktu wwc, asik dan menghibur kamii",  
                 "pesan": "semoga dilancarkan segala urusan dan jangan demis kak!"
+            },
+            {
+                "nama": "Muhammad Fahrul Aditya",
+                "nim": "121450156",
+                "umur": "22",
+                "asal": "Surakarta",
+                "alamat": "Sukarame",
+                "hobbi": "Badminton, melukis, minum kopo",
+                "sosmed": "@shrul.pdf",
+                "kesan": "bang fahrul saya kira abang prodi elektro",  
+                "pesan": "next kita foto lagi bang saat abang wisuda!"
             },
             {
                 "nama": "Berliana Enda Putri",
